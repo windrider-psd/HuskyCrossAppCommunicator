afdd7537e9f351941529ae11b0013e3655d9b502
Christian Lemos <christianlualemosc@hotmail.com>
2019-07-04 13:02:38 -0300

    Using server

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ let lodash = require('lodash')
 
 /**
  * @typedef {number} HuskyOperationType
-**/
+ **/
 /**
  * @typedef {number} HuskyResponseStatus
-**/
-
+ **/
+/**
+ * @typedef {number} HuskyMessageType
+ **/
 
 /**
  * @enum {HuskyOperationType} 
@@ -28,6 +30,21 @@ let RESPONSESTATUS = {
 }
 
 /**
+ * @enum {HuskyMessageType} 
+ */
+let MESSAGETYPE = {
+    COMMAND: 0,
+    RESPONSE: 1,
+}
+
+
+/**
+ * @typedef HuskyMessage
+ * @property {HuskyMessageType} messageType
+ * @property {String} arg
+ */
+
+/**
  * @typedef HuskyError
  * @property {String} code
  * @property {String} message
@@ -39,7 +56,7 @@ let RESPONSESTATUS = {
  * @property {String} commandPath
  * @property {Number} operationType
  * @property {String} arg
-*/
+ */
 
 /**
  * @typedef HuskyResponse
@@ -49,22 +66,43 @@ let RESPONSESTATUS = {
  */
 
 /**
- * @typedef WaitObject
+ * @typedef ResponseWaitObject
  * @property {Number} id
- * @property {WriteCallback} callback
+ * @property {ResponseCallback} callback
  */
 
 /**
- * @callback WriteCallback
+ * @callback ResponseCallback
  * @param {Error} error
  * @param {HuskyResponse} response
  * @returns {void}
  */
 
 
+
 /**
- * 
+ * @callback OnCommandCallback
+ * @param {HuskyCommand} command 
+ * @param {Function} writeResponse
  */
+
+/**
+ * @typedef OnCommandWaitObject
+ * @property {String} commandPath
+ * @property {HuskyOperationType} operationType
+ * @property {OnCommandCallback} OnCommandCallback
+ */
+
+/**
+ * @callback WriteResponseCallback
+ * @param {HuskyResponseStatus} responseStatus
+ * @param {any} arg
+ */
+
+/**
+ * @callback OnReadyCallback
+ */
+
 class CrossAppCommunicator {
     /**
      * 
@@ -73,41 +111,103 @@ class CrossAppCommunicator {
     constructor(port) {
         this.nextCommandId = 1;
         /**
-         * @type {Array.<WaitObject>}
+         * @type {Array.<ResponseWaitObject>}
          */
-        this.waitObjects = []
+        this.responseWaitObjects = []
 
-        this.client = new net.Socket();
-        this.conntected = false
-        this.client.connect(port, ip.address(), () => {
+        /**
+         * @type {Array.<OnCommandWaitObject>}
+         */
+        this.onCommandWaitObjects = []
+
+        /**
+         * @type {Array.<OnReadyCallback>}
+         */
+        this.onReadyCallbacks = []
+
+        this.server = new net.Server();
+        this.connected = false
+        this.server.listen(port, ip.address(), () => {
 
         })
 
-        this.client.on('connect', () => {
-            this.conntected = true
-        })
+        this.server.on('connection', (client) => {
+            if (this.connected) {
+                client.end();
+                return
+            }
+            this.connected = true;
+            this.client = client;
 
+            this.client.on('data', (data) => {
 
-        this.client.on('data', (data) => {
-            let json = String(data);
-            /**
-             * @type {HuskyResponse}
-             */
-            let response = JSON.parse(json);
+                let json = String(data);
+                /**
+                 * @type {HuskyMessage}
+                 */
+                let message = JSON.parse(json);
+                if (message.messageType == MESSAGETYPE.RESPONSE) {
+                    /**
+                     * @type {HuskyResponse}
+                     */
+                    let response = JSON.parse(message.arg)
+                    /**
+                     * @type {Array.<ResponseWaitObject>}
+                     */
+                    let called = []
+                    lodash.forEach(this.responseWaitObjects, (w) => {
+                        if (w.id == response.commandId) {
+                            called.push(w)
+                            w.callback(null, response)
+                        }
+                    })
 
-            /**
-             * @type {Array.<WaitObject>}
-             */
-            let called = []
-            lodash.forEach(this.waitObjects, (w) => {
-                if (w.id == response.commandId) {
-                    called.push(w)
-                    w.callback(null, response)
+                    lodash.remove(this.responseWaitObjects, (w) => {
+                        return called.includes(w, 0)
+                    })
+                } else if (message.messageType == MESSAGETYPE.COMMAND) {
+                    /**
+                     * @type {HuskyCommand}
+                     */
+                    let command = JSON.parse(message.arg)
+
+                    let called = false;
+                    lodash.forEach(this.onCommandWaitObjects, (w) => {
+                        if (w.commandPath == command.commandPath && w.operationType == command.operationType) {
+
+                            let func = new WriteResponseFunction(command, this);
+
+                            w.OnCommandCallback(command, func.response)
+
+                            called = true;
+                        }
+                    })
+                    if (!called) {
+                        /**
+                         * @type {HuskyError}
+                         */
+                        let err = {
+                            code: 'NRT',
+                            message: "No route"
+                        }
+
+                        this.WriteResponse(command.commandId, RESPONSESTATUS.INVALID, err)
+                    }
+
                 }
             })
 
-            lodash.remove(this.waitObjects, (w) => {
-                return called.includes(w, 0)
+            this.client.on('end', () => {
+                this.client = null;
+                this.connected = false;
+            })
+            this.client.on('timeout', () => {
+                this.client = null;
+                this.connected = false;
+            })
+            this.client.on('error', () => {
+                this.client = null;
+                this.connected = false;
             })
         })
     }
@@ -116,20 +216,16 @@ class CrossAppCommunicator {
      * @param {String} commandPath 
      * @param {OPERATIONTYPE} operationType 
      * @param {any} arg 
-     * @param {WriteCallback} callback 
+     * @param {ResponseCallback} callback 
      */
     WriteCommand(commandPath, operationType, arg, callback) {
-
         if (typeof (arg) == "undefined") {
             callback(new Error("arg is undefined"), null)
-        }
-        else if (typeof (commandPath) != "string") {
+        } else if (typeof (commandPath) != "string") {
             callback(new Error("commandPath must be a string"), null)
-        }
-        else if (typeof (operationType) != "number") {
+        } else if (typeof (operationType) != "number") {
             callback(new Error("operationType must be a number"), null)
-        }
-        else {
+        } else {
 
             /**
              * @type {String}
@@ -139,12 +235,10 @@ class CrossAppCommunicator {
             if (typeof (arg) == "object") {
                 try {
                     argParam = JSON.stringify(arg);
-                }
-                catch (err) {
+                } catch (err) {
                     callback(err, null)
                 }
-            }
-            else {
+            } else {
                 argParam = String(arg)
             }
 
@@ -157,16 +251,121 @@ class CrossAppCommunicator {
                 commandPath: commandPath,
                 operationType: operationType
             }
-            this.waitObjects.push({ id: this.nextCommandId, callback: callback })
+            this.responseWaitObjects.push({
+                id: this.nextCommandId,
+                callback: callback
+            })
             this.nextCommandId++
-            this.client.write(JSON.stringify(command))
+
+            /**
+             * @type {HuskyMessage}
+             */
+            let message = {
+                arg: JSON.stringify(command),
+                messageType: MESSAGETYPE.COMMAND
+            }
+
+            this.client.write(JSON.stringify(message))
+        }
+    }
+
+    /**
+     * 
+     * @param {String} commandPath 
+     * @param {HuskyOperationType} operationType 
+     * @param {OnCommandCallback} callback 
+     */
+    OnCommand(commandPath, operationType, callback) {
+        if (typeof (commandPath) != "string") {
+            callback(new Error("commandPath must be a string"), null)
+        } else if (typeof (operationType) != "number") {
+            callback(new Error("operationType must be a number"), null)
+        } else {
+            /**
+             * @type {OnCommandWaitObject}
+             */
+            let waitObject = {
+                commandPath: commandPath,
+                operationType: operationType,
+                OnCommandCallback: callback
+            }
+
+            this.onCommandWaitObjects.push(waitObject);
+        }
+    }
+
+    /**
+     * 
+     * @param {Number} commandId 
+     * @param {HuskyResponseStatus} responseStatus 
+     * @param {any} arg 
+     */
+    WriteResponse(commandId, responseStatus, arg) {
+
+        /**
+         * @type {String}
+         */
+        let argParam
+
+        if (typeof (arg) == "object") {
+            argParam = JSON.stringify(arg);
+        } else {
+            argParam = String(arg)
+        }
+
+        /**
+         * @type {HuskyResponse}
+         */
+        let response = {
+            arg: argParam,
+            commandId: commandId,
+            responseStatus: responseStatus
+        }
+
+        /**
+         * @type {HuskyMessage}
+         */
+        let message = {
+            arg: JSON.stringify(response),
+            messageType: MESSAGETYPE.RESPONSE
+        }
+        this.client.write(JSON.stringify(message))
+    }
+
+    /**
+     * 
+     * @param {OnReadyCallback} callback 
+     */
+    OnReady(callback) {
+        this.onReadyCallbacks.push(callback)
+        if (this.connected) {
+            callback();
         }
     }
 }
 
 
+class WriteResponseFunction {
+    /**
+     * 
+     * @param {HuskyCommand} command 
+     * @param {CrossAppCommunicator} server 
+     */
+    constructor(command, server) {
+        this.command = command;
+
+        /**
+         * @type {WriteResponseCallback}
+         */
+        this.response = (responseStatus, arg) => {
+            server.WriteResponse(this.command.commandId, responseStatus, arg);
+        }
+    }
+}
+
 module.exports = {
     OPERATIONTYPE: OPERATIONTYPE,
     RESPONSESTATUS: RESPONSESTATUS,
+    MESSAGETYPE: MESSAGETYPE,
     CrossAppCommunicator: CrossAppCommunicator
 }
